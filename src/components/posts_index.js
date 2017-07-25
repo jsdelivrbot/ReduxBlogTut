857a9374688f81d6feaf3eeebc3369100db1a498
Warner Nielsen <bwn888@gmail.com>
2017-07-25 16:51:08 -0600

    Style add post button with some top margin

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -23,7 +23,7 @@ class PostsIndex extends Component {
     return (
       <div>
         <div className='text-xs-right'>
-          <Link className='btn btn-primary' to='/posts/new'>
+          <Link className='btn btn-primary add-post' to='/posts/new'>
             Add a post
           </Link>
         </div>
