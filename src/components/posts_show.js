044c0ff1a7d6ae16e14d96602bf760c34fbe4360
Warner Nielsen <bwn888@gmail.com>
2017-07-25 17:39:57 -0600

    Setup delete_post callback, style delete button

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { fetchPost } from '../actions';
-
+import { fetchPost, deletePost } from '../actions';
 
 class PostsShow extends Component {
   componentDidMount() {
     const { id } = this.props.match.params;
     this.props.fetchPost(id);
   }
+
+  onDeleteClick() {
+    const { id } = this.props.match.params;
+    this.props.deletePost(id, () => {
+      this.props.history.push('/');
+    });
+  }
+
   render() {
     const { post } = this.props;
 
@@ -20,6 +27,12 @@ class PostsShow extends Component {
       <div>
         <div className='text-xs-right'>
           <Link to='/' className='btn btn-primary back-to-index'>Back To Index</Link>
+          <button
+            className='btn btn-danger delete-post'
+            onClick={this.onDeleteClick.bind(this)}
+          >
+            Delete Post
+          </button>
         </div>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
@@ -33,4 +46,4 @@ function mapStateToProps({ posts }, ownProps) {
   return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
