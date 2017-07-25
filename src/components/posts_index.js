bc12066481b4b82c369afd311eb296439671f2ce
Warner Nielsen <bwn888@gmail.com>
2017-07-25 14:07:40 -0600

    Create link button to new posts form

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,7 +1,8 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPosts } from '../actions';
-import _ from 'lodash';
 
 class PostsIndex extends Component {
   componentDidMount() {
@@ -21,6 +22,11 @@ class PostsIndex extends Component {
   render() {
     return (
       <div>
+        <div className='text-xs-right'>
+          <Link className='btn btn-primary' to='/posts/new'>
+            Add a post
+          </Link>
+        </div>
         <h3>Posts</h3>
         <ul className='list-group'>
           { this.renderPosts() }
