44c7b0cf302e665a7ae7f3c340254e7f13f86778
Warner Nielsen <bwn888@gmail.com>
2017-07-25 12:26:26 -0600

    Create fetch_posts action, update package.json

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios form 'axios';
+
+export const FETCH_POSTS = 'fetch_posts';
+
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=FREDCOW8943';
+
+export function fetchPosts() {
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+
+  return {
+    type: FETCH_POSTS,
+    payload: request
+  };
+}
