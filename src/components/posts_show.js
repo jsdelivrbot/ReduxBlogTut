9d6b4170b5c24270a324609e2809d60bf3f3e915
Warner Nielsen <bwn888@gmail.com>
2017-07-25 17:21:12 -0600

    Render post to client

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -8,9 +8,17 @@ class PostsShow extends Component {
     this.props.fetchPost(id);
   }
   render() {
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <div>
-        Posts Show!
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
   }
